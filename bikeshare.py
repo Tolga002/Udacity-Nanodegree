c74cfbc105670f8387f8101be048775f340cca68
Tolgacan Yucel <tolgacan.yucel@turkcell.com.tr>
2021-01-02 02:41:42 +0300

    Print Texts for end users have changed.

--- bikeshare.py
+++ bikeshare.py
@@ -9,11 +9,7 @@ DAY_DATA = ['all', 'monday', 'tuesday', 'wednesday', 'friday', 'saturday', 'sund
 def get_filters():
     """Asks user to specify a city, month, and day to analyze.
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter"""
-
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello. Let\'s explore some US bikeshare data!')
     city_n = ''
 
     while city_n.lower() not in CITY_DATA:
